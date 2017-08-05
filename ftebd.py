73983d2b1692b4e32827ba7bb568b11e922c1419
syed abuthahir <abuvanth@gmail.com>
2017-08-05 16:05:25 +0530

    Update ftebd.py

--- ftebd.py
+++ ftebd.py
@@ -4,7 +4,6 @@ URL = "https://raw.githubusercontent.com/kishorek/Free-Tamil-Ebooks/master/books
 response = requests.get(URL)
 with open('books.xml', 'wb') as file:
     file.write(response.content)
-z=0
 e = xml.etree.ElementTree.parse('books.xml').getroot()
 for atype in e.findall('book'):
     header={'User-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/534.30 (KHTML, like Gecko) Ubuntu/11.04 Chromium/12.0.742.112 Chrome/12.0.742.112 Safari/534.30'}
@@ -12,5 +11,6 @@ for atype in e.findall('book'):
     data=requests.get(atype.find('pdf').text, stream=True,headers=header)
     with open(atype.find('link').text.rsplit('/',2)[1]+".pdf",'wb') as fil:
          fil.write(data.content)  
-    print "download book",z
-    z=z+1
+    print "downloading ",atype.find('link').text.rsplit('/',2)[1]
+else:
+     print "All Books Are Downloaded Successfully"
