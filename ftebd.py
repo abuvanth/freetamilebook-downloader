d2a2b54d88e86a32e84184597d64057eed4e9282
syed abuthahir <abuvanth@gmail.com>
2017-10-10 16:32:17 +0530

    Update ftebd.py
    if pdf is not available, download epub file

--- ftebd.py
+++ ftebd.py
@@ -8,9 +8,15 @@ e = xml.etree.ElementTree.parse('books.xml').getroot()
 for atype in e.findall('book'):
     header={'User-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/534.30 (KHTML, like Gecko) Ubuntu/11.04 Chromium/12.0.742.112 Chrome/12.0.742.112 Safari/534.30'}
     #print atype.find('link').text.rsplit('/',2)[1]
-    data=requests.get(atype.find('pdf').text, stream=True,headers=header)
-    with open(atype.find('link').text.rsplit('/',2)[1]+".pdf",'wb') as fil:
-         fil.write(data.content)  
+    
+    if atype.find('pdf').text is not None:
+       data=requests.get(atype.find('pdf').text, stream=True,headers=header)
+       with open(atype.find('link').text.rsplit('/',2)[1]+".pdf",'wb') as fil:
+           fil.write(data.content)  
+    else:
+       data=requests.get(atype.find('epub').text, stream=True,headers=header)
+       with open(atype.find('link').text.rsplit('/',2)[1]+".epub",'wb') as fil:
+           fil.write(data.content)  
     print "downloading ",atype.find('link').text.rsplit('/',2)[1]
 else:
-     print "All Books Are Downloaded Successfully"
+    print "All Books Are Downloaded Successfully"
